26f5efd4035123cb300d6f4bc4f54afa85a58cd4
danny <dannytakushi@gmail.com>
2025-03-19 11:26:35 -0500

    initial draft

--- query_console/Program.cs
+++ query_console/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using libplctag;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var tags = new Tag<TagInfoPlcMapper, TagInfo[]>()
+        {
+            Gateway = "192.168.1.100", // Replace with the actual IP address of your PLC
+            PlcType = PlcType.CompactLogix,
+            Protocol = Protocol.ab_eip,
+            Name = "@tags"
+        };
+
+        tags.Read();
+        Console.WriteLine("All tags in the PLC:");
+        foreach (var tag in tags.Value)
+        {
+            Console.WriteLine($"Name: {tag.Name}, Type: 0x{tag.Type:X}, Length: {tag.Length}");
+        }
+    }
+}
