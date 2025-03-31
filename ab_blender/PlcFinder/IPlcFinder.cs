a735676af9c7168be4cf3e0a177e18563303a58b
danny <dannytakushi@gmail.com>
2025-03-31 11:57:41 -0500

    forgotten nullabe-string argument update

--- ab_blender/PlcFinder/IPlcFinder.cs
+++ ab_blender/PlcFinder/IPlcFinder.cs
@@ -1,5 +1,5 @@
 
 public interface IPlcFinder{
-    public abstract Task FindPlc(string startIp, string endIp);
+    public abstract Task FindPlc(string? startIp, string? endIp);
     public abstract string[] GetPlcIps();
 }
