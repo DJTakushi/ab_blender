918cbf8020717764ad8ddcd609d747de4804eddd
danny <dannytakushi@gmail.com>
2025-03-31 11:48:12 -0500

    tag_manager constructed from  env-var; otherwise constructed  from  plc_ips

--- ab_blender/AbBlender.cs
+++ ab_blender/AbBlender.cs
@@ -11,13 +11,30 @@ public class AbBlender : BackgroundService
 {
     private readonly IRabbitMQConnectionManager _connectionManager;
     private readonly IPlcFinder _plcFinder;
-    private readonly TagManager _tag_manager = new();
+    private readonly TagManager _tag_manager; // TODO : refactor into list AND make interfaces
     private readonly Queue<string> _outputs = [];
 
     public AbBlender(IRabbitMQConnectionManager connectionManager, IPlcFinder plcFinder)
     {
         _connectionManager = connectionManager ?? throw new ArgumentNullException(nameof(connectionManager));
         _plcFinder = plcFinder ?? throw new ArgumentNullException(nameof(plcFinder));
+
+        string? plc_address_ev = EnvVarHelper.GetPlcAddress();
+        if (!string.IsNullOrEmpty(plc_address_ev))
+        {
+            _tag_manager = new TagManager(plc_address_ev, EnvVarHelper.GetPlcType(), EnvVarHelper.GetPlcProtocol());
+        }
+        else
+        {
+            _plcFinder.FindPlc(EnvVarHelper.GetStartIp(), EnvVarHelper.GetEndIp());
+            string[] plc_ips = _plcFinder.GetPlcIps();
+            if (plc_ips.Length == 0)
+            {
+                Console.WriteLine("No PLCs found from EnvVar nor from scanning; exiting");
+                Environment.Exit(1);
+            }
+            // _tag_manager = new TagManager(_plcFinder.GetPlcIps()[0], EnvVarHelper.GetPlcType(), EnvVarHelper.GetPlcProtocol());
+        }
         _tag_manager.load_tags();
     }
 
