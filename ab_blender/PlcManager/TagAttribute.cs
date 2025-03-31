d600ecfdc9be71634e384b3a4f029cd25f5363e6
danny <dannytakushi@gmail.com>
2025-03-31 17:08:02 -0500

    TagType moved to separate file

--- ab_blender/PlcManager/TagAttribute.cs
+++ ab_blender/PlcManager/TagAttribute.cs
@@ -1,15 +1,6 @@
 
 using libplctag;
 using libplctag.DataTypes;
-public enum TagType // TODO : confirm these values
-{
-    BOOL,
-    SINT = 193, //  193, addr, 3, 1 
-    INT = 195, // 195, adr, 3,2
-    DINT = 196, // expressed by vars with "Dint" in their name
-    REAL = 202, // confirmed in the field
-    STRING = 33633
-}
 
 // TODO : make an interface and factory to replace stubbing
 class TagAttribute(TagInfo tagInfo, string address, PlcType plc_type, Protocol plc_protocol)
---
+++ ab_blender/PlcManager/TagType.cs
@@ -0,0 +1,9 @@
+public enum TagType // TODO : confirm these values
+{
+    BOOL,
+    SINT = 193, //  193, addr, 3, 1 
+    INT = 195, // 195, adr, 3,2
+    DINT = 196, // expressed by vars with "Dint" in their name
+    REAL = 202, // confirmed in the field
+    STRING = 33633
+}
