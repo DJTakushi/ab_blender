bae36e0da477f8048ee3a215ad723701ce35d268
danny <dannytakushi@gmail.com>
2025-03-31 20:13:20 -0500

    _tagFactory member-var in AbBlender

--- ab_blender/AbBlender.cs
+++ ab_blender/AbBlender.cs
@@ -9,20 +9,22 @@ public static class AppInfo
 
 public class AbBlender : BackgroundService
 {
+    private readonly ITagAttributeFactory _tagFactory;
     private readonly IRabbitMQConnectionManager _connectionManager;
     private readonly IPlcFinder _plcFinder;
     private readonly Dictionary<string, PlcManager> _tag_managers = []; // TODO : make interfaces
     private readonly Queue<string> _outputs = [];
 
-    public AbBlender(IRabbitMQConnectionManager connectionManager, IPlcFinder plcFinder)
+    public AbBlender(ITagAttributeFactory _tagFactory, IRabbitMQConnectionManager connectionManager, IPlcFinder plcFinder)
     {
+        _tagFactory = _tagFactory ?? throw new ArgumentNullException(nameof(_tagFactory));
         _connectionManager = connectionManager ?? throw new ArgumentNullException(nameof(connectionManager));
         _plcFinder = plcFinder ?? throw new ArgumentNullException(nameof(plcFinder));
 
         string? plc_address_ev = EnvVarHelper.GetPlcAddress();
         if (!string.IsNullOrEmpty(plc_address_ev))
         {
-            _tag_managers.Add(plc_address_ev, new PlcManager(plc_address_ev, EnvVarHelper.GetPlcType(), EnvVarHelper.GetPlcProtocol()));
+            _tag_managers.Add(plc_address_ev, new PlcManager(_tagFactory, plc_address_ev, EnvVarHelper.GetPlcType(), EnvVarHelper.GetPlcProtocol()));
         }
         else
         {
@@ -35,7 +37,7 @@ public class AbBlender : BackgroundService
             }
             foreach (string plc_ip in plc_ips)
             {
-                _tag_managers.Add(plc_ip, new PlcManager(plc_ip, EnvVarHelper.GetPlcType(), EnvVarHelper.GetPlcProtocol()));
+                _tag_managers.Add(plc_ip, new PlcManager(_tagFactory, plc_ip, EnvVarHelper.GetPlcType(), EnvVarHelper.GetPlcProtocol()));
             }
         }
         foreach (KeyValuePair<string, PlcManager> entry in _tag_managers)
